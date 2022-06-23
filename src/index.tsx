b5fd86524849a3a6a3ad861a1ec67c54912bbfdc
Antti Niinikoski <antti.niinikoski@robu.fi>
2022-06-23 14:05:10 +0300

    Add eslint and airbnb style check

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => {
+function App() {
   return <h1>Hello, world!</h1>;
-};
+}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
