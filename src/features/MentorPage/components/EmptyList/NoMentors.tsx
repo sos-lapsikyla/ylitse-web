a227de5efbf2aef30727a82dd862bf12e2e79e85
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2024-12-07 18:07:39 +0200

    Make mobile view of empty mentor list components

--- src/features/MentorPage/components/EmptyList/NoMentors.tsx
+++ src/features/MentorPage/components/EmptyList/NoMentors.tsx
@@ -1,30 +1,45 @@
+import { useGetLayoutMode } from '@/hooks/useGetLayoutMode';
 import { useTranslation } from 'react-i18next';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { palette } from '@/components/constants';
 import { Text } from '@/components/Text/Text';
 
 const NoMentors = () => {
   const { t } = useTranslation('mentors');
+  const { isMobile } = useGetLayoutMode();
 
   return (
-    <Container>
-      <Text variant="h2">{t('empty.title')}</Text>
-      <Text>{t('empty.description')}</Text>
+    <Container isMobile={isMobile}>
+      <CenteredText variant="h2">{t('empty.title')}</CenteredText>
+      <CenteredText>{t('empty.description')}</CenteredText>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ isMobile: boolean }>`
   align-items: center;
   background-color: ${palette.white};
   border-radius: 10px;
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
-  height: 159px;
+  height: 10rem;
   justify-content: center;
-  width: 606px;
+  width: 38rem;
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          margin-top: -1rem;
+          width: 90vw;
+        `
+      : css`
+          width: 38rem;
+        `}
+`;
+
+const CenteredText = styled(Text)`
+  text-align: center;
 `;
 
 export default NoMentors;
--- src/features/MentorPage/components/EmptyList/SearchTips.tsx
+++ src/features/MentorPage/components/EmptyList/SearchTips.tsx
@@ -1,16 +1,18 @@
+import { useGetLayoutMode } from '@/hooks/useGetLayoutMode';
 import { useTranslation } from 'react-i18next';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { palette } from '@/components/constants';
 import { Text } from '@/components/Text/Text';
 
 const SearchTips = () => {
   const { t } = useTranslation('mentors');
+  const { isMobile } = useGetLayoutMode();
 
   return (
-    <Container>
+    <Container isMobile={isMobile}>
       <Text variant="h2">{t('empty.tips.title')}</Text>
-      <InnerContainer>
+      <InnerContainer isMobile={isMobile}>
         <Text variant="blueBox">{t('empty.tips.tip1')}</Text>
         <Text variant="blueBox">{t('empty.tips.tip2')}</Text>
         <Text variant="blueBox">{t('empty.tips.tip3')}</Text>
@@ -19,19 +21,35 @@ const SearchTips = () => {
   );
 };
 
-const Container = styled.div`
-  align-items: center;
+const Container = styled.div<{ isMobile: boolean }>`
   background-color: ${palette.white};
-  border-radius: 10px;
-  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
-  height: 423px;
+  gap: 1.5rem;
+  height: 26.5rem;
   justify-content: center;
-  width: 642px;
+  padding: 0 0 0 3rem;
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          padding-top: 2rem;
+          width: 100vw;
+        `
+      : css`
+          border-radius: 10px;
+          box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
+          width: 40rem;
+        `}
 `;
-const InnerContainer = styled.div`
-  width: 580px;
+const InnerContainer = styled.div<{ isMobile: boolean }>`
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          width: 90vw;
+        `
+      : css`
+          width: 36rem;
+        `}
 `;
 
 export default SearchTips;
