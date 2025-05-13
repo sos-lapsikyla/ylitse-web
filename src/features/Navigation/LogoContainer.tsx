11ee348a181c123bdbb3ca4471f25f3bb38976a8
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2025-05-13 18:44:32 +0300

    Use new ylitse-logo

--- src/features/Navigation/LogoContainer.tsx
+++ src/features/Navigation/LogoContainer.tsx
@@ -1,41 +1,26 @@
 import styled from 'styled-components';
 
-import links from '@/static/links.json';
-import SvgLogo from '@/static/img/ylitse-logo.svg';
-import Text from '@/components/Text';
-import { useTranslation } from 'react-i18next';
+import SvgLogo from '@/static/img/ylitse-logo-small-new.svg';
 import { MOBILE_TRESHOLD } from '@/components/constants';
 import Link from '@/components/Link';
 
 export const LogoContainer = () => {
-  const { t } = useTranslation('common');
-
   return (
     <Container>
-      <Link url={links.sosLapsikylaYlitseUrl}>
+      <Link url={'/'}>
         <Logo />
-        <YlitseText variant="logo" color="white">
-          {t('navigation.logo')}
-        </YlitseText>
       </Link>
     </Container>
   );
 };
-
-const YlitseText = styled(Text)`
-  @media screen and (max-width: 650px) and (min-width: ${MOBILE_TRESHOLD}px) {
-    display: none;
-    margin-left: 4%;
-  }
-`;
 
 const Logo = styled.div`
   background-color: transparent;
   background-image: url(${SvgLogo});
   background-repeat: no-repeat;
   background-size: contain;
-  height: 40px;
-  width: 40px;
+  height: 37px;
+  width: 143px;
 `;
 
 const Container = styled.div`
