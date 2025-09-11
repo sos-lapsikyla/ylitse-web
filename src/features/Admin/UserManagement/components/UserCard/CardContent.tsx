3b4639fa821a5735cec105875c717273b4c44cb3
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2025-09-11 12:36:47 +0300

    Display correct info for usercards

--- src/features/Admin/UserManagement/components/UserCard/CardContent.tsx
+++ src/features/Admin/UserManagement/components/UserCard/CardContent.tsx
@@ -2,6 +2,7 @@ import { useGetLayoutMode } from '@/hooks/useGetLayoutMode';
 import styled from 'styled-components';
 import { ManagedUser } from '../../models';
 import Text from '@/components/Text';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   managedUser: ManagedUser;
@@ -9,21 +10,34 @@ type Props = {
 
 export const CardContent: React.FC<Props> = ({ managedUser }) => {
   const { isMobile } = useGetLayoutMode();
+  const { t } = useTranslation('users');
 
   return (
     <Card isMobile={isMobile}>
-      <Text variant="h3">{'Julkinen käyttäjänimi'}</Text>
-      <Text variant="p">{managedUser.nickname}</Text>
-      <Text variant="h3">{'Luotu '}</Text>
-      <Text variant="p">
-        {new Date(managedUser.created).toLocaleString('fi-EU', {
-          year: 'numeric',
-          month: 'numeric',
-          day: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-        })}
-      </Text>
+      <TextGroup>
+        <Text variant="boldBaloo">{t('card.userName')}</Text>
+        <UserInfoText variant="p">{managedUser.username}</UserInfoText>
+      </TextGroup>
+      <TextGroup>
+        <Text variant="boldBaloo">{t('card.publicName')}</Text>
+        <UserInfoText variant="p">{managedUser.nickname}</UserInfoText>
+      </TextGroup>
+      <TextGroup>
+        <Text variant="boldBaloo">{t('card.email')}</Text>
+        <UserInfoText variant="p">{managedUser.email}</UserInfoText>
+      </TextGroup>
+      <TextGroup>
+        <Text variant="boldBaloo">{t('card.created')}</Text>
+        <UserInfoText variant="p">
+          {new Date(managedUser.created).toLocaleString('fi-EU', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        </UserInfoText>
+      </TextGroup>
     </Card>
   );
 };
@@ -35,6 +49,15 @@ const Card = styled.div<{ isMobile: boolean }>`
   gap: 1.5rem;
   justify-content: space-between;
   padding: ${({ isMobile }) => (isMobile ? '1.5rem' : '2.5rem')};
+`;
+
+const UserInfoText = styled(Text)`
+  margin: 0;
+`;
+
+const TextGroup = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default CardContent;
