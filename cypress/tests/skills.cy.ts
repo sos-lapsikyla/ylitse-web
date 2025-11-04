d3f8cb1f5ecfa8997b21f1a653fcce52f66f3cad
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2025-11-04 13:00:28 +0200

    Test skill page

--- cypress/tests/skills.cy.ts
+++ cypress/tests/skills.cy.ts
@@ -1,12 +1,11 @@
 import { generateTotp } from 'cypress/support/api';
-import { accounts } from 'cypress/fixtures/accounts';
 import { api } from '../support/api';
 
 const SUPERADMIN_USER = Cypress.env('apiUser') || 'admin';
 const SUPERADMIN_PASS = Cypress.env('apiPass') || '';
 const SUPERADMIN_MFA = Cypress.env('mfaSecret') || '';
 
-const mentor = accounts.mentors[0];
+type Skill = { id: number; name: string };
 
 describe('Skills', () => {
   beforeEach(() => {
@@ -14,6 +13,11 @@ describe('Skills', () => {
       SUPERADMIN_USER,
       SUPERADMIN_PASS,
       generateTotp(SUPERADMIN_MFA).token,
+    );
+
+    // Mock the RTK Query backend request
+    cy.intercept('GET', '**/api/skills', { fixture: 'skills.json' }).as(
+      'getSkills',
     );
   });
 
@@ -23,12 +27,29 @@ describe('Skills', () => {
 
   it('lists skills', () => {
     cy.get('[href="/topics"]').click();
+
+    cy.wait('@getSkills');
+
     cy.location('pathname').should('eq', '/topics');
     cy.getByText('Täällä voit hallinnoida mentoreiden aiheita.').should(
       'be.visible',
     );
-    cy.getByText(mentor.skills[0]).should('be.visible');
-    cy.getByText(mentor.skills[1]).should('be.visible');
-    cy.getByText(mentor.skills[2]).should('be.visible');
+    // skills are displayed
+    cy.fixture('skills.json').then(({ resources }) => {
+      resources.forEach((skill: Skill) => {
+        cy.getByText(skill.name).should('be.visible');
+      });
+    });
+  });
+
+  it('deletes skill after confirmation', () => {
+    cy.get('[href="/topics"]').click();
+    cy.wait('@getSkills');
+    cy.location('pathname').should('eq', '/topics');
+    cy.getByText('React').click();
+    cy.getByText('Poista aihe').should('be.visible');
+    cy.getByText('Poista', 'button').click();
+    cy.wait('@getSkills');
+    cy.getByText('Aiheen poistaminen onnistui.').should('be.visible');
   });
 });
